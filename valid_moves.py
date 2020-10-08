c016f1d384d700c9e594d72b0bb8f42b089de121
dani3l554 <71512424+dani3l554@users.noreply.github.com>
2020-10-08 13:16:23 +0000

    Very dodgy validity checks. WIll be fixed late

--- valid_moves.py
+++ valid_moves.py
@@ -1,2 +1,26 @@
-def check_valid(name,prev_pos,next_pos):
-	return True
+def check_valid(name,prev_pos,next_pos,board):
+	if name == "":
+		return False
+	colour = name[0]
+	if board[next_pos[1]][next_pos[0]] != "":
+		if board[next_pos[1]][next_pos[0]][0] == colour:
+			return False
+	if name[1] == "r":
+		if prev_pos[0] == next_pos[0] or prev_pos[1] == next_pos[1]:
+			return True
+		return False
+	elif name[1] == "q":
+		return True
+	elif name[1] == "b":
+		if prev_pos[0] - next_pos[0] == prev_pos[1] - next_pos[1] or prev_pos[0] + next_pos[0] == prev_pos[1] + next_pos[1]:
+			return True
+		return False
+	elif name[1] == "n":
+		if prev_pos[0] - next_pos[0] in (-2,-1,1,2):
+			if prev_pos[0] - next_pos[0] == -2 or prev_pos[0] - next_pos[0] == 2:
+				if prev_pos[1] - next_pos[1] in (1,-1):
+					return True
+			else:
+				if prev_pos[1] - next_pos[1] in (2,-2):
+					return True
+			return False
